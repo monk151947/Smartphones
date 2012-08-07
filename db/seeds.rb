90bd1692ef3a81b31f5b091a84e79ec8e83065cd
jagdish <monk151947@gmail.com>
2012-08-07 20:21:11 +0530

    changed layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,3 +36,35 @@ Product.create(:title => "HTC One X",
 Brace yourself for lightning-fast web browsing, remarkable picture quality and seamless gaming visuals thanks to the powerful quad-core processor. You will love the minimalist design and the camera that captures every moment (even in low light) with crisp, vivid, beautiful photos. It also lets you take a photo while shooting HD video.</p>},
 :image_url => '/assets/Onex.jpg',
 :price => 650)
+
+Product.create(:title => 'HTC Wildfire S',
+  :description =>
+    %{<p>A superphone that lets you share. Everything. Surf the web in style with multi-window browsing, pinch to zoom with auto text reflow, and Flash video support. The HTC Wildfire S lets you take great shots with the 5 MP camera with flash, tag your friends in the pics and instantly post them to social networks for all to enjoy. Explore thousands of apps on Android Market and send your favorite ones to friends - so everyone can get in on the fun.
+</p>},
+  :image_url => '/assets/htc.jpg',
+  :price => 250)
+
+Product.create(:title => 'HTC One V',
+  :description =>
+    %{<p>
+The HTC One V features a unique curvedchin design that makes it easy to hold and a distinctive accessory to your life. But don not be deceived by its compact size. Like all One series phones, it packs an impressive list of features including a best in class camera.
+</p>},
+  :image_url => '/assets/onev.jpg',
+  :price => 300)
+
+
+Product.create(:title => 'HTC Exploror ',
+  :description =>
+    %{<p>
+HTC Explorer is the simply smarter phone that puts YOU in control. An intuitive, easy to use lockscreen and homescreen let you access your most important information and tasks. Enjoy web browsing similar to your desktop. Keep in touch with loved ones even easier with email designed for easy navigation and quick reply. And the People Widget lets you group up your favorite people and view all your communication options on one screen for simple access.
+</p>},
+  :image_url => '/assets/Exp.jpg',
+  :price => 150)
+
+Product.create(:title => "HTC One X",
+:description =>
+%{<p>
+Brace yourself for lightning-fast web browsing, remarkable picture quality and seamless gaming visuals thanks to the powerful quad-core processor. You will love the minimalist design and the camera that captures every moment (even in low light) with crisp, vivid, beautiful photos. It also lets you take a photo while shooting HD video.</p>},
+:image_url => '/assets/Onex.jpg',
+:price => 650)
+
